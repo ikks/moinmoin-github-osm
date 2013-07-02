a45d7677fe8e5952835a6aa0a43029731b971e8a
Igor Támara <igor@tamarapatino.org>
2013-07-01 20:00:33 -0500

    Added macro

---
+++ macro/GithubJson.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#Author: Igor Támara igor@tamarapatino.org
+#Date: 01/07/2013
+#No warranties.
+"""
+  MoinMoin - Geo Json github
+  Embeds a geojson map from github inside moinmoin
+
+  Inputs: Receieves a Github URL containing a geojson file, it must be
+  of the kind embed
+
+  For instance:
+  <<GithubJson(https://embed.github.com/view/geojson/ikks/moinmoin-github-osm/master/samples/colombiatermal.geojson)>>
+
+  would show a nice map with openstreetmap as it base layers and all
+  the geometries in a nice leaflet container by github and mapbox.
+
+  For instructions on how to build the URL for embedding, take a look at
+  https://help.github.com/articles/mapping-geojson-files-on-github
+"""
+
+
+def execute(macro, args):
+    if not args.startswith('https://embed.github.com/view/geojson/'):
+        return "you must offer something that starts with https://embed.github..."
+    return macro.formatter.rawHTML('<script src="{0}"></script>'.format(args))
